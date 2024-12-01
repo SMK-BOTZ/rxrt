abaf4d6b53380ccb68566b95d1891585199e7e4c
SMK BOTZ <163844722+SMK-BOTZ@users.noreply.github.com>
2024-12-02 03:36:37 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -5,7 +5,7 @@
 from os import environ
 
 class Config:
-UPDATES_CHANNEL = "-1002235661888"
+UPDATES_CHANNEL = "-1002197951128"
 
 API_ID = int(environ.get("API_ID", "28243586"))
 API_HASH = environ.get("API_HASH", "4022d5686b9b7a7cf8891205921a0ab3")
